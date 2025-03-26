80640accda43df8125c66b1058044d80279eabbd
lait <lait.zhang@gmail.com>
2025-03-27 01:23:07 +0800

    fix

--- rbac-config/src/main/java/org/dows/rbac/config/RbacScanner.java
+++ rbac-config/src/main/java/org/dows/rbac/config/RbacScanner.java
@@ -3,21 +3,19 @@ package org.dows.rbac.config;
 import cn.hutool.json.JSONUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.dows.rbac.api.annotation.Menu;
 import org.dows.rbac.config.a.RbacConfig;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.type.filter.AnnotationTypeFilter;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
 import org.springframework.web.servlet.mvc.method.RequestMappingInfoHandlerMapping;
 
 import java.lang.reflect.Method;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @description: </br>
@@ -41,30 +39,6 @@ public class RbacScanner {
     private List<String> scanPackages;
 
     /**
-     * 扫描菜单
-     *
-     * @param basePackage
-     * @return
-     */
-    public Set<Class<?>> scanMenu(String basePackage) {
-        ClassPathScanningCandidateComponentProvider provider =
-                new ClassPathScanningCandidateComponentProvider(false);
-        provider.addIncludeFilter(new AnnotationTypeFilter(Menu.class));
-        Set<BeanDefinition> components = provider.findCandidateComponents(basePackage);
-
-        Set<Class<?>> classes = new HashSet<>();
-        for (BeanDefinition component : components) {
-            try {
-                classes.add(Class.forName(component.getBeanClassName()));
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
-            }
-        }
-        return classes;
-    }
-
-
-    /**
      * 扫描并返回所有需要权限处理的接口资源
      * 这里模拟扫描，借助 org.springframework.web.servlet.mvc.method.RequestMappingInfoHandlerMapping
      */
@@ -86,58 +60,6 @@ public class RbacScanner {
                     }
                 }
             }
-            /*// 拿到类(模块)上的权限注解（可填可不填）
-            Class<?> beanType = handlerMethod.getBeanType();
-            Menu menu = beanType.getAnnotation(Menu.class);
-            Uri moduleUri = beanType.getAnnotation(Uri.class);
-            // 拿到接口方法上的权限注解
-            Method method1 = handlerMethod.getMethod();
-            Uri methodUri = method1.getAnnotation(Uri.class);
-            Operation operation = method1.getAnnotation(Operation.class);
-            String name;
-            // package.class.method or classAuth.code.methodAuth.code
-            String code;
-            if (operation != null) {
-                name = operation.summary();
-            } else {
-                name = method1.getName();
-            }
-            if (moduleUri != null) {
-                code = moduleUri.code();
-            } else {
-                code = beanType.getName();
-            }
-
-            if (methodUri != null) {
-                code = code + "." + methodUri.code();
-            } else {
-                code = code + "." + method1.getName();
-            }
-
-            // 拿到该接口方法的请求方式(GET、POST等)
-            Set<RequestMethod> methods = info.getMethodsCondition().getMethods();
-            // 如果一个接口方法标记了多个请求方式，权限id是无法识别的，不进行处理
-            if (methods.size() != 1) {
-                return;
-            }
-            // 将请求方式和路径用`:`拼接起来，以区分接口。比如：GET:/user/{id}、POST:/user/{id}
-            String httpMethod = methods.toArray()[0].toString();
-//            String path = httpMethod + ":" + info.getPathPatternsCondition().getPatterns().toArray()[0];
-            String path = info.getPathPatternsCondition().getPatterns().toArray()[0] + "";
-            // 将权限名、资源路径、资源类型组装成资源对象，并添加集合中
-            InitUriResources resource = InitUriResources.builder()
-                    .path(path)
-                    .name(name)
-                    .code(code)
-                    .appId(appId)
-                    .method(httpMethod)
-                    .menuName(menu != null ? menu.name() : "")
-                    .packageName(handlerMethod.getBeanType().getPackageName())
-                    //.id() // 用code 代替确保唯一性
-                    .build();*/
-           // MethodSignature methodSignature = extracted(info, handlerMethod);
-            //list.add(resource);
-//            log.info("akjdlajd:{}",handlerMethod.getBeanType());
         });
         return list;
     }
