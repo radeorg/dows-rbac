bde81eabbb61b372addfeae96a2d32d411fa5c82
Libiyou <biyou.li@hosjoy.com>
2025-03-30 02:08:19 +0800

    RbacApi接口

--- rbac-biz/src/main/java/org/dows/rbac/biz/RbacApiBiz.java
+++ rbac-biz/src/main/java/org/dows/rbac/biz/RbacApiBiz.java
@@ -1,5 +1,6 @@
 package org.dows.rbac.biz;
 
+import com.mybatisflex.core.query.QueryChain;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.dows.rbac.api.InitResources;
@@ -9,10 +10,17 @@ import org.dows.rbac.api.RoleResourceResponse;
 import org.dows.rbac.api.admin.request.FindRbacResourcesRequest;
 import org.dows.rbac.api.admin.request.SaveRbacRoleRequest;
 import org.dows.rbac.api.admin.response.*;
+import org.dows.rbac.entity.RbacMenuEntity;
+import org.dows.rbac.entity.RbacPermissionEntity;
+import org.dows.rbac.entity.RbacRoleEntity;
+import org.dows.rbac.entity.RbacUriEntity;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -20,17 +28,79 @@ import java.util.Map;
 public class RbacApiBiz implements RbacApi {
     @Override
     public List<RbacRoleResponse> getRoleInstanceByRoleIds(String appId, List<Long> roleIds) {
-        return RbacApi.super.getRoleInstanceByRoleIds(appId, roleIds);
+        List<RbacRoleResponse> responseList = new ArrayList<>();
+        for(Long roleId : roleIds){
+            List<RbacRoleEntity> rbacRoleEntities = QueryChain.of(RbacRoleEntity.class)
+                    .eq(RbacRoleEntity::getAppId, appId, Objects.nonNull(appId))
+                    .eq(RbacRoleEntity::getRbacRoleId, roleId, Objects.nonNull(roleId)).list();
+            if(Objects.isNull(rbacRoleEntities) || rbacRoleEntities.size() == 0){
+                continue;
+            }
+
+            for(RbacRoleEntity item : rbacRoleEntities) {
+                RbacRoleResponse response = new RbacRoleResponse();
+                BeanUtils.copyProperties(item, response);
+                responseList.add(response);
+            }
+
+        }
+        return responseList;
     }
 
     @Override
     public List<RbacUriResponse> getAllUrisByAppId(String appId) {
-        return RbacApi.super.getAllUrisByAppId(appId);
+        List<RbacUriResponse> response = new ArrayList<>();
+        if(Objects.isNull(appId)){
+            return response;
+        }
+        List<RbacUriEntity> rbacRoleEntities = QueryChain.of(RbacUriEntity.class)
+                .eq(RbacUriEntity::getAppId, appId, Objects.nonNull(appId)).list();
+        BeanUtils.copyProperties(rbacRoleEntities, response);
+
+        return response;
     }
 
     @Override
     public List<RoleResourceResponse> getUrisByRoleIds(String appId, List<Long> roleIds) {
-        return RbacApi.super.getUrisByRoleIds(appId, roleIds);
+        List<RoleResourceResponse> responseList = new ArrayList<>();
+        for(Long roleId : roleIds){
+            List<RbacPermissionEntity> rbacPermissionEntities = QueryChain.of(RbacPermissionEntity.class)
+                    .eq(RbacPermissionEntity::getAppId, appId, Objects.nonNull(appId))
+                    .eq(RbacPermissionEntity::getRbacRoleId, roleId, Objects.nonNull(roleId)).list();
+            if(Objects.isNull(rbacPermissionEntities) || rbacPermissionEntities.size() == 0){
+                continue;
+            }
+
+            RoleResourceResponse response = new RoleResourceResponse();
+            response.setRoleId(roleId);
+            List<String> authority = new ArrayList<>();
+            for(RbacPermissionEntity item : rbacPermissionEntities) {
+                Long resourceId = item.getResourceId();
+                //资源类型[0:接口，1:菜单]
+                if(Objects.nonNull(resourceId) &&  item.getResourceType() == 0){
+                    List<RbacUriEntity> rbacruiEntities = QueryChain.of(RbacUriEntity.class)
+                            .eq(RbacUriEntity::getRbacUriId, resourceId, Objects.nonNull(resourceId)).list();
+                    if(Objects.isNull(rbacruiEntities) || rbacruiEntities.size() == 0){
+                        continue;
+                    }
+                    for(RbacUriEntity uriItem : rbacruiEntities){
+                        authority.add(uriItem.getJavaMethod());
+                    }
+                }else if(Objects.nonNull(resourceId) &&  item.getResourceType() == 1){
+                    List<RbacMenuEntity> rbacMenuEntities = QueryChain.of(RbacMenuEntity.class)
+                            .eq(RbacMenuEntity::getRbacMenuId, resourceId, Objects.nonNull(resourceId)).list();
+                    if(Objects.isNull(rbacMenuEntities) || rbacMenuEntities.size() == 0){
+                        continue;
+                    }
+                    for(RbacMenuEntity uriItem : rbacMenuEntities){
+                        authority.add(uriItem.getMenuCode());
+                    }
+                }
+            }
+            response.setAuthority(authority);
+            responseList.add(response);
+        }
+        return responseList;
     }
 
     @Override
